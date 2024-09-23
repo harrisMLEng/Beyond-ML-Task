cc587f6a4e586716aba50478e3408122ad19ec79
harrisDLEng <harris47naeem@gmail.com>
2024-09-23 14:29:43 +0100

    fix: add doc strings

--- src/KNN.py
+++ src/KNN.py
@@ -9,12 +9,44 @@ from src.model import Model
 
 @dataclass
 class KNN(Model):
+    """
+    A class used to represent a K-Nearest Neighbors model.
+
+    Attributes
+    ----------
+    model : KNeighborsClassifier
+        The K-Nearest Neighbors classifier.
+    dataset : Iris
+        The Iris dataset to be used for training and prediction.
+
+    Methods
+    -------
+    train() -> None
+        Trains the K-Nearest Neighbors model using the Iris dataset.
+    predict(data : ndarray)
+        Predicts the class labels for the given data using the trained model.
+    """
     model : KNeighborsClassifier
     dataset : Iris
     
     def train(self) -> None:
+        """
+        Trains the K-Nearest Neighbors model using the Iris dataset.
+        """
         self.model.fit(self.dataset.X, self.dataset.y)
 
     def predict(self, data : ndarray):
+        """
+        Predicts the class labels for the given data using the trained model.
+
+        Parameters
+        ----------
+        data : ndarray
+            The input data for which the class labels need to be predicted.
+
+        Returns
+        -------
+        ndarray
+            The predicted class labels for the input data.
+        """
         return self.model.predict(data)
-    
--- src/iris_data.py
+++ src/iris_data.py
@@ -12,14 +12,40 @@ class Iris:
     X : ndarray
     y : ndarray
 
-
 class Preprocess(StandardScaler):
+    """
+    A class used to preprocess the Iris dataset using StandardScaler.
+
+    Attributes
+    ----------
+    dataset : Iris
+        The Iris dataset to be preprocessed.
+
+    Methods
+    -------
+    preprocess:
+        Preprocesses the dataset by fitting and transforming it using StandardScaler.
+    """
     dataset : Iris
 
     def __init__(self, dataset : Iris):
+        """
+        Parameters
+        ----------
+        dataset : Iris
+            The Iris dataset to be preprocessed.
+        """
         self.dataset = dataset
         super().__init__()
 
     @cached_property
     def preprocess(self):
+        """
+        Preprocesses the dataset by fitting and transforming it using StandardScaler.
+
+        Returns
+        -------
+        ndarray
+            The preprocessed dataset.
+        """
         return super().fit(self.dataset.X).transform(self.dataset.X)
